4955b261d0d16ff15261200ec971bcfa97024c2d
Nikola <nikolablagojevic848@gmail.com>
2023-02-02 17:44:35 +0100

    Making UserService for creating user and storing user info

---
+++ Kriptografija(Projekat)/Model/User.cs
@@ -0,0 +1,35 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kriptografija_Projekat_.Model
+{
+    public class User
+    {
+        private X509Certificate _cert;
+        private AsymmetricCipherKeyPair _keyPair;
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public AsymmetricCipherKeyPair KeyPair { get { return _keyPair; } }
+        public X509Certificate Cert { get { return _cert; } }
+
+        public User(string username, string password, string email, X509Certificate cert)
+        {
+            _cert = cert;
+            Username = username;
+            Password = password;
+            Email = email;
+            using (var reader = File.OpenText(ConfigurationManager.AppSettings["Main"]! + @"\" + username + ".pem"))
+                _keyPair = (AsymmetricCipherKeyPair)new PemReader(reader).ReadObject();
+        }
+    }
+}
--- Kriptografija(Projekat)/Service/UserService.cs
+++ Kriptografija(Projekat)/Service/UserService.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Kriptografija_Projekat_.Model;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.X509;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +15,13 @@ namespace Kriptografija_Projekat_.Service
     public class UserService
     {
         public UserService() { }
+
+        public UserFile UploadFile(User user, string filePath)
+        {
+            FileSystemService fsService = new FileSystemService();
+            UserFile result = fsService.AddFile(user.Username, filePath, user.KeyPair);
+            return result;
+        }
 
          
     }
